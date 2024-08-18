76aaddd2356974c3ab0592b7a8c9f522ca73b8bb
chaitanyasai508 <chaitanyasai508@gmail.com>
2024-08-18 23:27:09 +0530

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -7,13 +7,7 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <header>
-      <h1>React Photo Album | Lightbox</h1>
-      <a href="https://github.com/igordanchenko/react-photo-album" target="_blank" rel="noreferrer noopener">
-        GitHub
-      </a>
-      <a href="https://react-photo-album.com/" target="_blank" rel="noreferrer noopener">
-        Docs
-      </a>
+      <h1>Ved Photo Albums</h1>
     </header>
     <main>
       <App />
